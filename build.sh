c2072a55ae910d45426fe3170cbdd5bbfbc2cd74
marmooo <marmooo@users.noreply.github.com>
2022-04-30 10:06:57 +0900

    minify js

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cp node_modules/sql.js-httpvfs/dist/sqlite.worker.js docs/sql.js-httpvfs/
 cp node_modules/sql.js-httpvfs/dist/sqlite.worker.js.map docs/sql.js-httpvfs/
 cp node_modules/sql.js-httpvfs/dist/sql-wasm.wasm docs/sql.js-httpvfs/
 deno bundle src/index.js > docs/index.js
+minify docs/index.js -o docs/index.js
