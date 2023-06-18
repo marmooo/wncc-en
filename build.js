dafae6698e89fea757a901ec46e5969e4065aa5e
marmooo <marmooo@users.noreply.github.com>
2023-06-19 00:13:41 +0900

    cleanup

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { bundle } from "https://deno.land/x/emit/mod.ts";
 
-const url = new URL(import.meta.resolve("./src/index.js"));
+const url = new URL(import.meta.resolve(Deno.args[0]));
 const { code } = await bundle(url);
 console.log(code);
