8ada838733986a35a1de879f3d70d0840b879e9a
marmooo <marmooo@users.noreply.github.com>
2023-08-31 09:36:58 +0900

    cleanup

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ minify -r docs -o .
 cp node_modules/sql.js-httpvfs/dist/sqlite.worker.js docs/sql.js-httpvfs/
 cp node_modules/sql.js-httpvfs/dist/sqlite.worker.js.map docs/sql.js-httpvfs/
 cp node_modules/sql.js-httpvfs/dist/sql-wasm.wasm docs/sql.js-httpvfs/
-deno run -A build.js ./src/index.js > docs/index.js
+deno run -A bundle.js ./src/index.js > docs/index.js
 minify docs/index.js -o docs/index.js
